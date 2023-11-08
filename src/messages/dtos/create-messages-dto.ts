5be1fc0ca890ea458e5f7759fb63b2c149c8f8cc
amandaregina <amandaregina.s@hotmail.com>
2023-11-08 17:17:54 -0300

    testing swagger

--- src/messages/dtos/create-messages-dto.ts
+++ src/messages/dtos/create-messages-dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsString } from "class-validator";
 
 export class CreateMessageDto {
+    @ApiProperty({
+     description: 'O content é usado para registrar uma mensagem',
+     example: 'EU TO LOUCO? EU SO LOUCO?'
+    })
     @IsString()
     content: string;
 }
