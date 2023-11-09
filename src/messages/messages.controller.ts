8ee5c7a164b817b09c82c128ab79f3ad0644f5fc
amandaregina <amandaregina.s@hotmail.com>
2023-11-09 13:55:11 -0300

    Nest Architecture: Service and Repositories

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,22 +1,36 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-messages-dto';
-
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
-    @Get()
-    listMessages(){
+  constructor(public messagesService: MessagesService) {}
 
+  @Get()
+  listMessages() {
+    return this.messagesService.findAll();
+  }
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    return this.messagesService.create(body.content);
+  }
+
+  @Get('/:id')
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('message not found');
     }
 
-    @Post()
-    createMessage(@Body() body: CreateMessageDto){
-        console.log(body)
-    }
-
-    @Get('/:uid')
-    getMessage(@Param('uid') uid: string){
-        console.log(uid)
-    }
+    return message
+  }
 }
-
